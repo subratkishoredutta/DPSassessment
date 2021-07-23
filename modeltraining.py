afaa17c7cbef9ddccc0c3ea44a43c9c2e5f69b0d
SUBRAT KISHORE DUTTA <subratkishoredutta1234@gmail.com>
2021-07-23 13:35:03 +0530

    Update modeltraining.py

--- modeltraining.py
+++ modeltraining.py
@@ -24,4 +24,4 @@ pickle.dump(regressor, open('model.pkl','wb'))
 
 # Loading model to compare the results
 model = pickle.load(open('model.pkl','rb'))
-print(int(model.predict([[0.95,0]])[0]))
+print(int(model.predict([[20,10]])[0]))
